7997ddeaca52d0ac3a3dc3cb388fd38be441f53f
kappa-spec <kappa20134@gmail.com>
2026-02-19 17:48:41 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,16 +1,11 @@
+/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: '/uv/service/',
     bare: '/bare/',
-    encodeUrl: function(str) {
-        if (!str) return str;
-        return encodeURIComponent(str.split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char).join(''));
-    },
-    decodeUrl: function(str) {
-        if (!str) return str;
-        let [input, ...search] = str.split('?');
-        return decodeURIComponent(input).split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char).join('') + (search.length ? '?' + search.join('?') : '');
-    },
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
